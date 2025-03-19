dbc8e9a08441705436b490be8ea5ccf625b04887
Dom Andolino <42012173+dom-andolino@users.noreply.github.com>
2025-03-19 15:33:05 -0400

    Update test_python_script.py
    refactor

--- test_python_script.py
+++ test_python_script.py
@@ -58,33 +58,33 @@ def get_result_names(query_str):
 
 if __name__ == '__main__': 
 
+    #init variables
     api_key = os.environ.get("GOOGLE_API_KEY")
     service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
-    celeb_nm_output_path = 'data/test_python_script_output.csv'
-
-    all_result_names_df = pd.DataFrame(columns=["celeb_nm", 'cat', 'cat_rnk']) #initialize empty df
-    query_list = ['Actors', 'Netflix', 'NFL', 'YouTube', 'Bitcoin', 'Politician']
+    output_path = 'data/test_python_script_output.csv'
+    output_df = pd.DataFrame(columns=["celeb_nm", 'cat', 'cat_rnk']) #init empty df
+    query_list = ['Actors', 'Netflix', 'NFL', 'YouTube', 'Bitcoin', 'Politician'] #TODO: store list in csv or DB
 
     for query in query_list:
-        #call api and get list of names for each query
+        #call api and get list of names for each query item
         print('\nGetting Names for: {}'.format(query))
-        result_df = get_result_names(query_str=query) #get df of names matching query
-        all_result_names_df = pd.concat([all_result_names_df, result_df], ignore_index=True) #append current df to all_df
+        query_df = get_result_names(query_str=query) #get df of names matching query
+        output_df = pd.concat([output_df, query_df], ignore_index=True) #add query_df to output_df
 
 
-    #add some extra calculated fields
-    all_result_names_df.sort_values(by = ['celeb_nm', 'cat_rnk', 'cat'], inplace = True)
-    all_result_names_df.reset_index(drop=True, inplace=True)
-    all_result_names_df['cat_rnk'] = all_result_names_df['cat_rnk'].apply(lambda x: int(x)) #needs to be an into for below transforms to work
-    all_result_names_df['num_cats'] = all_result_names_df.groupby(['celeb_nm'])['cat'].transform('count') # count of rows (ie number of serach terms the name came up in)
-    all_result_names_df['bst_cat_rnk'] = all_result_names_df.groupby(['celeb_nm'])['cat_rnk'].transform('min') # lowest rnk will be the best match
-    all_result_names_df['bst_cat_rnk_idx'] = all_result_names_df.groupby(['celeb_nm'])['cat_rnk'].transform('idxmin') #add index of best match so we can do below to get associated cat
-    all_result_names_df['bst_cat'] = all_result_names_df.loc[all_result_names_df.bst_cat_rnk_idx, 'cat'].values #add best cat
+    #add calculated fields
+    output_df.sort_values(by = ['celeb_nm', 'cat_rnk', 'cat'], inplace = True)
+    output_df.reset_index(drop=True, inplace=True)
+    output_df['cat_rnk'] = output_df['cat_rnk'].apply(lambda x: int(x)) #needs to be an into for below transforms to work
+    output_df['num_cats'] = output_df.groupby(['celeb_nm'])['cat'].transform('count') # count of rows (ie number of serach terms the name came up in)
+    output_df['bst_cat_rnk'] = output_df.groupby(['celeb_nm'])['cat_rnk'].transform('min') # lowest rnk will be the best match
+    output_df['bst_cat_rnk_idx'] = output_df.groupby(['celeb_nm'])['cat_rnk'].transform('idxmin') #add index of best match so we can do below to get associated cat
+    output_df['bst_cat'] = output_df.loc[output_df.bst_cat_rnk_idx, 'cat'].values #add best cat
 
 
-    #dedupp, remove extra cols
-    all_result_names_df.drop(['cat','cat_rnk', 'bst_cat_rnk_idx'], axis=1, inplace=True)
-    all_result_names_df.drop_duplicates(subset = 'celeb_nm', keep = 'first', inplace = True)
+    # remove unneeded cols and duplicate rows
+    output_df.drop(['cat','cat_rnk', 'bst_cat_rnk_idx'], axis=1, inplace=True)
+    output_df.drop_duplicates(subset='celeb_nm', keep='first', inplace=True)
 
-
-    all_result_names_df.to_csv(celeb_nm_output_path, index=False, encoding='utf-8', sep=',') #save to csv
+    #save results to csv
+    output_df.to_csv(output_path, index=False, encoding='utf-8', sep=',') 
